f5b12089b9f85ef62078e612495e5350dea1ef95
Gausiya <gausiya1403@gmail.com>
2022-09-27 21:02:04 +0530

    initial commit

---
+++ pattern2.cpp
@@ -0,0 +1,30 @@
+// hollow rectangle
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col, i, j;
+    cout << "enter rows & columns\n";
+    cin >> row >> col;
+
+    for (i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= col; j++)
+        {
+            if (i == 1 || i == row)
+            {
+                cout << "*";
+            }
+            else if (j == 1 || j == col)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
